5f8b4bba6251f694559fe07d8a7a1278ef1fd7cb
Roel Magdaleno <roelmagdaleno@gmail.com>
2026-09-16 14:08:11 -0600

    style(serialized): add tabindex for accessibility on JSON display

--- resources/views/livewire/serialized.blade.php
+++ resources/views/livewire/serialized.blade.php
@@ -68,7 +68,7 @@
                 <p class="text-sm text-zinc-600 dark:text-zinc-300">This result is not retained by Unserialize.</p>
             </div>
             <div class="relative">
-                <pre class="mt-2 rounded-lg p-6" data-lang="json" id="clipText">{{ $result }}</pre>
+                <pre tabindex="0" class="mt-2 overflow-x-auto rounded-lg p-6" data-lang="json" id="clipText">{{ $result }}</pre>
                 <button class="copyBtn hidden md:block" type="button" title="Copy JSON to clipboard" aria-label="Copy JSON to clipboard" data-clipboard-target="#clipText" data-clipboard-event="result-copied">
                     <svg class="h-5 w-5 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true"><path d="M8 3a1 1 0 011-1h2a1 1 0 110 2H9a1 1 0 01-1-1z"></path><path d="M6 3a2 2 0 00-2 2v11a2 2 0 002 2h8a2 2 0 002-2V5a2 2 0 00-2-2 3 3 0 01-3 3H9a3 3 0 01-3-3z"></path></svg>
                 </button>
